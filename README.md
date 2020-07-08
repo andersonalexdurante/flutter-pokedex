156441f1bfefd8baa2583792357aa96005f194ff
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-07-08 19:07:57 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter Pokédex
+
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Pokédex',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold());
+  }
+}
